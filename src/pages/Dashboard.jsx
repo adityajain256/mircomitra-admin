a282fee2e35fe30cbed1de3bd8157553f91d8026
adityajain256 <jainaditya25522@gmail.com>
2026-03-28 15:27:40 +0530

    don't know

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,18 +1,32 @@
+import React, { useEffect, useState } from "react";
+import api from "../utils/api.js";
+
 export default function Dashboard() {
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const res = await api.get("/dashboard");
+      setData(res);
+    } catch (error) {
+      console.log("Error fetching dashboard data:", error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  });
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
-
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <Card title="Total Users" value="1200" />
-        <Card title="Active Users" value="900" />
-        <Card title="Reports" value="25" />
+        <Card title="Total Users" value={data?.totalUsers || "0"} />
+        <Card title="Active Users" value={data?.totalJobs || "0"} />
+        <Card title="Reports" value={data?.reports || "0"} />
       </div>
 
       <div className="bg-white mt-6 p-4 rounded shadow">
         <h3 className="text-lg font-semibold mb-4">Recent Activity</h3>
         <p className="text-gray-500">No recent activity</p>
       </div>
-
     </div>
   );
 }
